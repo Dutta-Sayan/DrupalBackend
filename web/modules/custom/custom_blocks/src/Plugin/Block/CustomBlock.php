c87535c3d82dc3862666ad0368bb3f667f3a863e
Dutta-Sayan <sayan.dutta@innoraft.com>
2024-08-12 11:43:19 +0530

    FT2-257: Added dependency injection for current user  service.

--- web/modules/custom/custom_blocks/src/Plugin/Block/CustomBlock.php
+++ web/modules/custom/custom_blocks/src/Plugin/Block/CustomBlock.php
@@ -3,6 +3,9 @@
 namespace Drupal\custom_blocks\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a Custom block.
@@ -13,7 +16,48 @@ use Drupal\Core\Block\BlockBase;
  *   category = @Translation("First custom block"),
  * )
  */
-class CustomBlock extends BlockBase {
+class CustomBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Contains object of current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Constructs a Drupalist object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   The current_user.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    AccountInterface $current_user,
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_user')
+    );
+  }
 
   /**
    * Prints the role of the currently logged in user.
@@ -23,12 +67,12 @@ class CustomBlock extends BlockBase {
    */
   public function build(): array {
     // Array containing user roles.
-    $roles = implode(',', \Drupal::currentUser()->getRoles());
+    $roles = implode(',', $this->currentUser->getRoles());
     return [
       '#type' => 'markup',
       '#markup' => $this->t('Welcome @role', ['@role' => $roles]),
       '#cache' => [
-        'tags' => ['user:' . \Drupal::currentUser()->id()],
+        'tags' => ['user:' . $this->currentUser->id()],
       ],
     ];
   }
